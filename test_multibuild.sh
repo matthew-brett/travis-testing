eced613c8cea7d1e9e865559d038005be0032831
Matthew Brett <matthew.brett@gmail.com>
2017-11-11 16:42:36 +0000

    Closer still to original repo

--- test_multibuild.sh
+++
@@ -1,9 +0,0 @@
-# Test multibuild utilities
-source common_utils.sh
-source tests/utils.sh
-
-source tests/test_common_utils.sh
-source tests/test_fill_submodule.sh
-
-# Exit 1 if any test errors
-barf
