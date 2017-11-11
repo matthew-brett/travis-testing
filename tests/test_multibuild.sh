5463b90f8df176630c6c1f22378fd405a2d1fb66
Matthew Brett <matthew.brett@gmail.com>
2017-11-11 17:14:45 +0000

    Remove common_utils

--- tests/test_multibuild.sh
+++ tests/test_multibuild.sh
@@ -1,5 +1,10 @@
-# Test multibuild utilities
-source common_utils.sh
+function rm_mkdir {
+    # Remove directory if present, then make directory
+    local path=$1
+    if [ -z "$path" ]; then echo "Need not-empty path"; exit 1; fi
+    if [ -d "$path" ]; then rm -rf $path; fi
+    mkdir $path
+}
 
 rm_mkdir tmp_repos
 cd tmp_repos
